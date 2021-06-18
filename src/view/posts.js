2150ad46d1fcb890f1d8b96b045ddaad65548e8d
vetneka <vetneka@yandex.ru>
2021-06-18 12:11:33 +0500

    (review)refactor: move map-callback to separate function

--- src/view/posts.js
+++ src/view/posts.js
@@ -1,3 +1,37 @@
+const createPostItem = (post, viewedPosts, i18nextInstance) => {
+  const item = document.createElement('li');
+  item.classList.add(
+    'd-flex',
+    'list-group-item',
+    'justify-content-between',
+    'align-items-start',
+  );
+
+  const link = document.createElement('a');
+
+  // Styles for font-weight are duplicated,
+  // because hexlet-check (tests) requires bootstrap 4.6 while application used 5.0
+  const linkFontWeights = (viewedPosts.has(post.id))
+    ? ['fw-normal', 'font-weight-normal']
+    : ['fw-bold', 'font-weight-bold'];
+
+  link.classList.add(...linkFontWeights);
+  link.href = post.link;
+  link.textContent = post.title;
+
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-primary', 'btn-sm', 'ms-2');
+  button.type = 'button';
+  button.dataset.bsToggle = 'modal';
+  button.dataset.bsTarget = '#postPreviewModal';
+  button.dataset.postId = post.id;
+  button.textContent = i18nextInstance.t('buttons.postPreview');
+
+  item.append(link, button);
+
+  return item;
+};
+
 export default (state, elements, i18nextInstance) => {
   const { postsContainer } = elements;
 
@@ -13,40 +47,9 @@ export default (state, elements, i18nextInstance) => {
   const postsList = document.createElement('ul');
   postsList.classList.add('list-group');
 
-  const postsListItems = state.posts.map((post) => {
-    const item = document.createElement('li');
-    item.classList.add(
-      'd-flex',
-      'list-group-item',
-      'justify-content-between',
-      'align-items-start',
-    );
+  const postItems = state.posts
+    .map((post) => createPostItem(post, state.uiState.viewedPostsIds, i18nextInstance));
 
-    const link = document.createElement('a');
-
-    // Styles for font-weight are duplicated,
-    // because hexlet-check (tests) requires bootstrap 4.6 while application used 5.0
-    const linkFontWeights = (state.uiState.viewedPostsIds.has(post.id))
-      ? ['fw-normal', 'font-weight-normal']
-      : ['fw-bold', 'font-weight-bold'];
-
-    link.classList.add(...linkFontWeights);
-    link.href = post.link;
-    link.textContent = post.title;
-
-    const button = document.createElement('button');
-    button.classList.add('btn', 'btn-primary', 'btn-sm', 'ms-2');
-    button.type = 'button';
-    button.dataset.bsToggle = 'modal';
-    button.dataset.bsTarget = '#postPreviewModal';
-    button.dataset.postId = post.id;
-    button.textContent = i18nextInstance.t('buttons.postPreview');
-
-    item.append(link, button);
-
-    return item;
-  });
-
-  postsList.append(...postsListItems);
+  postsList.append(...postItems);
   postsContainer.append(header, postsList);
 };
